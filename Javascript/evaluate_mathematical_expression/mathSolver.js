545b89ce870571d9c9c33e1da66f427ddd1558f9
karudedios <c.dedios@outlook.com>
2014-12-30 16:34:17 -0400

    Code for solving Kata

---
+++ Javascript/evaluate_mathematical_expression/mathSolver.js
@@ -0,0 +1,57 @@
+function resolve(x) {
+	if (x.match(/-*[0-9\.]+/g).length == 1) { return +x.match(/-*[0-9\.]+/); }
+	var copy = x.slice();
+	var numbers = [];
+	var action = "";
+	var result = 0;
+	var match;
+
+	while (match = copy.match(/-*[0-9\.]+/)) {
+		var value = match[0];
+		numbers.push(+value);
+		copy = copy.replace(value, "");
+	}
+
+	copy = copy.replace(/[\( \)]/g, '');
+	action = copy.length == 1 ? copy.match(/[-+*\/]/g)[0] : undefined;
+
+	switch(action) {
+		case "-": result = numbers[0] - numbers[1]; break;
+		case "/": result = numbers[0] / numbers[1]; break;
+		case "*": result = numbers[0] * numbers[1]; break;
+		default : result = numbers[0] + numbers[1]; break;
+	}
+
+	return result;
+}
+
+var calc = function (str) {
+	var copy = str.slice()
+	var patterns = [
+		{ pattern: /\(-*[0-9\.]+\)/, count: 0 },
+		{ pattern: /\({2}-*[0-9\.]+[\s]*[-+*\/]+[\s]*-*[0-9\.]+\){2}/, count: 0 },
+		{ pattern: /\(-*[0-9\.]+[\s]*[-+*\/]+[\s]*-*[0-9\.]+\)/, count: 0 },
+		{ pattern: /-*[0-9\.]+[\s]*[*\/]+[\s]*-*[0-9\.]+/, count: 0 },
+		{ pattern: /-*[0-9\.]+[\s]*[-+]+[\s]*-*[0-9\.]+/ , count: 0 }
+	];
+	var pattern;
+	var index = -1;
+	var match;
+
+	while (true) {
+		index++;
+		if (index == patterns.length || !isNaN(+copy)) break;
+
+		var element = patterns[index];
+		while (match = copy.match(element.pattern)) {
+			copy = copy.replace(match[0], resolve(match[0])).replace("--", "");
+			element.count++;
+		}
+
+		if (element.count > 0) {
+			patterns.forEach(function (x) { x.count = 0; })
+			index = -1;
+		}
+	}
+	return +copy;
+}
