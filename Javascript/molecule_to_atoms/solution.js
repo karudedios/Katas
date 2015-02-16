54fc60a3733e4241cf82783a7984b1c577fbd1a9
karudedios <c.dedios@outlook.com>
2015-02-16 09:42:37 -0400

    Molecule to Atoms refactor

--- Javascript/molecule_to_atoms/solution.js
+++ Javascript/molecule_to_atoms/solution.js
@@ -1,61 +1,6 @@
-/*var parseMolecule = function (molecule) {
-  var atoms = "Mg H O K S N B".split(" ");
-	var match;
-	while (match = molecule.match(/[\(\\[\{]([^(\{\[)]+)[\}\]\)]([0-9]*)/)) {
-		molecule = molecule.replace(match[0], simpleHandler(match[1], match[2]));
-	}
-
-	var singleAtoms = atoms.filter(function (x) {
-		return (molecule.indexOf(x) > -1);
-	}).reduce(function (o, v) {
-		o[v] = 0;
-		return o;
-	}, {});
-
-	for (var name in singleAtoms) {
-		var initPos = molecule.indexOf(name) + name.length;
-		var c = '';
-		var n = 0;
-		while (n = molecule[initPos]) {
-			initPos++;
-			if (!isNaN(n)) { c += n }
-			else { break }
-		}
-		singleAtoms[name] += +c || 1; 
-	}
-	return singleAtoms;
-}
-
-var simpleHandler = function (molecule, quantity) {
-  var atoms = "Mg H O K S N B".split(" ");
-	var toReturn = "";
-	var splitPattern = new RegExp(
-		atoms.filter(function(x) {
-			return molecule.indexOf(x) > -1
-		}).map(function (x) {
-			return "(?=" + x + ")"
-		}).join("|"), "g"
-	);
-
-	var ob = {};
-	var singleAtoms = molecule.split(splitPattern);
-	for (var i in singleAtoms) {
-		var atom = singleAtoms[i].replace(/[0-9]/g, '');
-		ob[atom] = ob[atom] || 0;
-		ob[atom] += (+singleAtoms[i].replace(/[a-z]/gi, '') || 1) * quantity;;
-	}
-
-	for (var name in ob) {
-		toReturn += name + ob[name];
-	}
-
-	return toReturn;
-}*/
-
-
 var parseMolecule = function (molecule) {
 	var match;
-  var lookPattern = /(?=As|Be|Cu|Co|Mg|Mo|Fe|Pd|B|H|O|K|N|S|C|P)/;
+  var lookPattern = /(?=[A-Z][a-z]*)/;
   var inclusionPattern = /[\(\\[\{]([^(\{\[)\}\)\]]+)[\}\]\)]([0-9]*)/;
 	
 	while (match = molecule.match(inclusionPattern)) {
