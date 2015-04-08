be65631ff149e756570a577163bd11b890048f43
karudedios <c.dedios@outlook.com>
2015-04-08 17:58:18 -0400

    Ascii85 Encoding start

---
+++ Javascript/ascii85_encoding/solution-spec.js
@@ -0,0 +1,27 @@
+require("./solution.js");
+
+describe("ASCII85", function(){
+	describe("Encoding", function() {
+		it("should encode small and simple words", function () {
+
+		});
+		it("should encode moderate words", function () {
+
+		});
+		it("should encode big and complicated words", function () {
+
+		});
+	});
+
+	describe("Decoding", function() {
+		it("should decode small and simple words", function () {
+
+		});
+		it("should decode moderate words", function () {
+
+		});
+		it("should decode big and complicated words", function () {
+
+		});
+	});
+});
---
+++ Javascript/ascii85_encoding/solution.js
@@ -0,0 +1,7 @@
+String.prototype.toAscii85 = function() {
+  // encode this string as ASCII85
+}
+
+String.prototype.fromAscii85 = function() {
+  // decode this string from ASCII85
+}
