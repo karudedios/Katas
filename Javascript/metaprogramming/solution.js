1d79b7610fd58b4c30c4a714bdae20cd38ba36cf
karudedios <c.dedios@outlook.com>
2015-04-09 09:57:54 -0400

    Tremendous progress for Metaprogramming Kata!

--- Javascript/metaprogramming/solution.js
+++ Javascript/metaprogramming/solution.js
@@ -1,67 +1,78 @@
-var defgeneric = function(name, idx, cases) {
-	idx = idx || 0;
-	var scenarios = cases && cases.slice() || [];
+var defgeneric = function(name) {
+	var idx = 0;
+	var scenarios = [];
 
 	var valid = function(scenario, args) {
-		var splitScenarioTypes = scenario.types.split(",");
-		var valid = splitScenarioTypes.length === args.length;
+		var splitDiscriminators = scenario.discriminator.split(",");
+		var valid = splitDiscriminators.length === args.length;
 
-		for (var idx = 0; idx < splitScenarioTypes.length && valid; idx++) {
-			var type = splitScenarioTypes[idx];
-			var arg = args[idx];
+		for (var i = 0; i < splitDiscriminators.length && valid; i++) {
+			var type = splitDiscriminators[i];
+			var arg = args[i];
 
-			valid = valid && (type == "*" || (arg).constructor.name == type);
+			valid = valid && (type == "*" || (arg).constructor.name == type || typeof (arg) == type || arg == null && t == null);
 		}
 
 		return valid;
 	};
 
-	if (idx == 3) {
-		return function tryApply() {
-			var args = [].slice.call(arguments, 0);
-			var scenario = scenarios.shift();
+	var tryApply = function tryApply() {
+		var args = [].slice.call(arguments, 0);
+		var method = tryApply.findMethod.apply(this, args);
 
-			if (scenario) {
-				if (valid(scenario, args)) {
-					return (scenario.method.apply(this, args));
-				} else {
-					return tryApply.apply(this, args);
-				}
-			} else {
-				var types = args.map(function(x) { return typeof(x); });
-				throw ("No method found for " + (types.length > 0 ? types : "null"));
-			}
+		if (method) {
+			return method.apply(this, args);
 		}
-	} else {
-		return {
-			defmethod: function(types, method) {
-				var cScenario = scenarios.slice();
-				cScenario.push({ types: types, method: method });
-				return defgeneric(name, ++idx, cScenario);
+
+		var types = args.map(function(x) { return typeof(x); });
+		throw ("No method found for " + name + " with args: " + (types.length > 0 ? types : "null"));
+	};
+
+	tryApply.defmethod = function(discriminator, fn, combination) {
+		scenarios.push({id: ++idx, discriminator: discriminator, method: fn });
+		return tryApply;
+	}
+
+	tryApply.removemethod = function(discriminator, combination) {
+		scenarios = scenarios.filter(function(x) { return x.discriminator != discriminator && x.combination != combination; });
+		return tryApply;
+	}
+
+	tryApply.findMethod = function() {
+		var args = [].slice.call(arguments, 0);
+		var cScenarios = scenarios.slice();
+
+		for (var idx = 0; idx < cScenarios.length; idx++) {
+			var scenario = cScenarios[idx];
+
+			if (valid(scenario, args)) {
+				return scenario.method;
 			}
 		}
 	}
+
+	return tryApply;		
 }
 
-var append = defgeneric("append")
-	.defmethod('Array,Array', function (a,b) { return a.concat(b); })
-	.defmethod('*,Array', function (a,b) { return [a].concat(b); })
-	.defmethod('Array,*', function (a,b) { return a.concat([b]); });
 
-if (append([1, 1, 1, 1], 6) + '' == '1,1,1,1,6') {
-	var sum =	defgeneric("sum")
-		.defmethod("Number,Number", function(a, b) { return a + b })
-		.defmethod("Number,String", function(a, b) { return a + int.Parse(b); })
-		.defmethod("String,Number", function(a, b) { return int.Parse(a) + b; });
+var append = defgeneric("append");
+append.defmethod('Array,Array', function (a,b) { return a.concat(b); });
+append.defmethod('*,Array', function (a,b) { return [a].concat(b); });
+append.defmethod('Array,*', function (a,b) { return a.concat([b]); });
 
-	if(sum(1, 2) == 3) {
-		var power = defgeneric("power")
-			.defmethod("Number", function(a) { return a * a; })
-			.defmethod("String", function(a) { return +a * +a; })
-			.defmethod("*", function() { throw "Invalid Operation"; ;});
+console.log(append([1], 6));
 
-		if(power(6) == 36) {
-			console.log("----------------NAILED IT----------------")
-		}
-	}
-}
+
+var sum =	defgeneric("sum");
+sum.defmethod("Number,Number", function(a, b) { return a + b });
+sum.defmethod("Number,*", function(a, b) { return a + +b; });
+sum.defmethod("*,Number", function(a, b) { return +a + b; });
+
+console.log(sum(1, 2));
+
+
+var power = defgeneric("power");
+power.defmethod("Number", function(a) { return a * a; });
+power.defmethod("String", function(a) { return +a * +a; });
+
+console.log(power([0]))
