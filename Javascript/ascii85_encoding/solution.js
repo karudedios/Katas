fa1b2c76abd5a72a9f41aa245f0b02036bb0ce56
Carlos de Dios <c.dedios@outlook.com>
2015-07-21 22:34:28 -0400

    Dealing with Corner-cases for ASCII85 encoding/decoding

--- Javascript/ascii85_encoding/solution.js
+++ Javascript/ascii85_encoding/solution.js
@@ -1,8 +1,12 @@
 'use strict';
 
-String.prototype.toAscii85 = function() {  
+String.prototype.toAscii85 = function() {
+  if (!this) return;
+
+  let charsToRemove = 1;
+
   let FourToAscii = (st) => {
-    let ignoreFromIndex = 5;
+    while (st.length % 4 != 0 && ++charsToRemove) st = st + "\0";
     let toBinary = (str) => str.charCodeAt(0).toString(2);
     let eightBits = (str) => {
       let strs = str.slice();
@@ -11,29 +15,32 @@ String.prototype.toAscii85 = function() {
       };
       return strs;
     };
-    while (st.length % 4 != 0 && ignoreFromIndex--) st = st + "\0";
-    
+
     let n = parseInt(st.split('').map(toBinary).map(eightBits).join(''), 2);
-
     let convertToAscii85 = ((num) => (power) => 33 + (Math.floor(num / Math.pow(85, power)) % 85));
-    let intToChar = (x, index) => (ignoreFromIndex > index) ? String.fromCharCode(x) : '';
+    let intToChar = (x, index) => String.fromCharCode(x);
 
-    return [4, 3, 2, 1, 0].map(convertToAscii85(n)).map(intToChar).join('');
+    return [4, 3, 2, 1, 0].map(convertToAscii85(n)).map(intToChar).join('').replace(/!!!!!/g, 'z');
   };
 
-  return `<~${this.match(/.{1,4}/g).map(FourToAscii).join('')}~>`;
+  return `<~${(this.match(/.{1,4}/g).map(FourToAscii).join('') + ' ').slice(0, -charsToRemove)}~>`;
 }
 
 String.prototype.fromAscii85 = function() {
+  if (!this) return;
+
+  let charsToRemove = 1;
   let fiveToText = (st) => {
-    let convertFromAscii85 = 
-    return st.split('').map((x) => x.charCodeAt(0) - 33);
+    while(st.length < 5 && charsToRemove++) st += 'u';
+    let convertFromAscii85 = (acc, x, i) => acc + ((x.charCodeAt(0) - 33) * Math.pow(85, (4) - i));
+    let bits = st.split('').reduce(convertFromAscii85, 0).toString(2);
+    while(bits.length % 4 != 0) bits = "0" + bits;
+    return bits.match(/.{8}/g).map(x => parseInt(x, 2)).map(x => String.fromCharCode(x)).join('');
   }
 
-  return this.match(/[^<~>]{1,5}/g).map(fiveToText);
+  return (this.replace(/z/g, '!!!!').match(/[^<~>]{1,5}/g).map(fiveToText).join('') + ' ').slice(0, -charsToRemove);
 }
 
 console.log(
   "somewhat difficult".toAscii85(),
-  "<~F)Po,GA(E,+Co1uAnbatCif~>".fromAscii85()
-)
+  "<~F)Po,GA(E,+Co1uAnbatCif~>".fromAscii85());
