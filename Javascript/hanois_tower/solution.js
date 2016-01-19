952a8efd8616f6aef8865d6204d02fc88dfa5aec
Carlos de Dios <c.dedios@outlook.com>
2016-01-19 17:26:37 -0400

    Refactoring Hanoi's tower

--- Javascript/hanois_tower/solution.js
+++ Javascript/hanois_tower/solution.js
@@ -1,69 +1,38 @@
-Array.prototype.head = function() {
-	return this[0] ||Infinity ;
-}
-
 var hanoi = function(towerSize) {
-	var defFloor = Array.apply(null, { length:towerSize }).map(function (x, i) { return i + 1; });
-	var tower = [defFloor, [], []];
+	var tower = [Array.from({ length: towerSize }, (_, i) => i + 1), [], []];
 
-	var moveBetween = function(PointA, pointB) {
-		var cA = PointA.slice();
-		var cB = pointB.slice();
-
-		if (cA.head() < cB.head())
-			cB.unshift(cA.shift());
-		else
-			cA.unshift(cB.shift());
-
-		return [cA, cB];
-	}
+	var moveBetween = ([x, ...xs], [y, ...ys]) =>
+		((x || Infinity) < (y || Infinity))
+			? [xs, [x, ...(y ? [y] : []), ...ys]]
+			: [[y, ...(x ? [x] : []), ...xs], ys];
 
 	function solve(tower) {
 		var attempts = 0;
+		var [A, B, C] = tower.slice();
 		var isOdd = (tower[0].length % 2) != 0;
-		var cTower = tower.slice();
-		var A = cTower[0];
-		var B = cTower[1];
-		var C = cTower[2];
 
-		return function() {
-			if (C + '' === tower[0] + '') {
-				throw Error;
-			} else {
-				attempts = (attempts > 2 ? 0 : attempts) + 1;
+		return function(att = (++attempts % 3)) {
+			if (C + '' === tower[0] + '')
+				return undefined;
 
-				var runAB = (attempts == 1 && !isOdd) || (attempts == 2 && isOdd)
-				var runAC = (attempts == 1 && isOdd) || (attempts == 2 && !isOdd)
+			if ((att == 2 && isOdd) || (att == 1 && !isOdd))
+				[A, B] = moveBetween(A, B);
+			else if ((att == 1 && isOdd) || (att == 2 && !isOdd))
+				[A, C] = moveBetween(A, C);
+			else
+				[B, C] = moveBetween(B, C);
 
-				if (runAB) {
-					var AB = moveBetween(A, B);
-					A = AB[0];
-					B = AB[1];
-				}
-				else if (runAC) {
-					var AC = moveBetween(A, C);
-					A = AC[0];
-					C = AC[1];
-				}
-				else {
-					var BC = moveBetween(B, C);
-					B = BC[0];
-					C = BC[1];
-				}
-
-				return [A, B, C];				
-			}
+			return [A, B, C];
 		}
 	}
 
 	return { nextStep: solve(tower), currentStep: function() { return tower; }};
 }
 
-var depth = 6;
+var depth = 3;
 var Tower = new hanoi(depth);
 
-for (var i = 0; i < Math.pow(2, depth) - 1; i++) {
+for (var i = 0; i < Math.pow(2, depth) - 1; i++)
 	console.log(Tower.nextStep());
-}
 
 module.exports.hanoi = hanoi;
