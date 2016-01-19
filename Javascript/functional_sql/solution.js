0c4af00ee29b5c88745dba234d3aa48ef02a644e
Carlos de Dios <c.dedios@outlook.com>
2016-01-19 17:28:53 -0400

    Retaking functional_sql
    Currenty there's inline testing, because I really want to see what
    happens with every change on the spot, don't want to have to change
    documents or write commands or anything. Unit Testing will be added
    later.

--- Javascript/functional_sql/solution.js
+++ Javascript/functional_sql/solution.js
@@ -1,75 +1,174 @@
 'use strict';
 
-var query = function(selectFn, collection, where, orderBy, groupBy, failure) {
-  if (!(this instanceof query)) return new query(selectFn, collection, where, orderBy , groupBy);
-  var defaultFn = function(x) { return x; }
-  var defaultFilter = function() { return true; }
+const query = (() => {
+  const compose = (f, g) => (...x) => g(f(...x));
+  const id = x => x;
 
-  this.select = function(q) {
-    return new query(q, collection, where, orderBy, groupBy, failure || (selectFn && "Duplicate SELECT"));
+  class Query {    
+    constructor(select, from, where, stack = id, group = 0) {
+      Object.defineProperty(this, '_from', { value: from });
+      Object.defineProperty(this, 'stack', { value: stack });
+      Object.defineProperty(this, '_whered', { value: where });
+      Object.defineProperty(this, '_grouped', { value: group });
+      Object.defineProperty(this, '_selected', { value: select });
+    }
+
+    from(...sets) {
+      if (this._from)
+        throw "Multiple FROM clauses";
+
+      return (sets.length === 1)
+        ? new Query(this._selected, sets[0], this._whered, this.stack, this._grouped)
+        : new JoinedQuery(this._selected, sets, this._whered, this.stack, this._grouped);
+    }
+
+    select(apply = id) {
+      if (this._selected)
+        throw "Multiple SELECT clauses";
+
+      const newStack = compose(this.stack, (arr) => arr.map(apply));
+      return new Query(1, this._from, this._whered, newStack, this._grouped);
+    }
+
+    where(predicate) {
+      const newStack = compose(this.stack, (arr) => arr.filter(predicate));
+      return new Query(this._selected, this._from, 1, newStack, this._grouped);
+    }
+
+    groupBy(...props) {
+      const groupBy = (prop, ...props) => (arr) => {
+        const groups = Object.keys(arr.map(prop).reduce((ob, x) => (ob[x] = 1) && ob, {}));
+
+        const innerGroupBy = (props, arr) => (props.length ? groupBy(...props) : id)(arr);
+
+        return groups.map(x => [x, innerGroupBy(props, arr.filter(y => prop(y) == x))]);
+      }
+
+      const newStack = compose(this.stack, groupBy(...props));
+      return new Query(this._selected, this._from, this._whered, newStack, 1);
+    }
+
+    orderBy(comparator) {
+      const newStack = compose(this.stack, (arr) => arr.sort(comparator));
+      return new Query(this._selected, this._from, this._whered, newStack, this._grouped);
+    }
+
+    having(predicate) {
+      if (!this._grouped)
+        throw "Cannot use HAVING clause before GROUP BY";
+
+      const having = (predicate) => (groups) => {
+        const [ [h, ...content] ] = groups.filter(predicate);
+
+        const traverseGroups = ([ [first, ...next] ]) =>
+          first && typeof first[0] == 'string' && first[1] instanceof Array
+            ? first[1].concat(traverseGroups([ next ])).sort()
+            : [];
+
+        return [[h, traverseGroups(content)]]
+      }
+
+      const newStack = compose(this.stack, having(predicate));
+      return new Query(this._selected, this._from, this._whered, newStack, this._grouped);
+    }
+
+    execute() {
+      return this.stack(this._from);
+    }
   }
 
-  this.from = function() {
-    var args = [].slice.call(arguments, 0);
-    args = args.length == 1 ? args[0] : args;
-    return new query(selectFn, args, where, orderBy, groupBy, failure || (collection && "Duplicate FROM"));
+  class JoinedQuery extends Query {
+    constructor(select, [first, ...next], where, stack = id, group = 0) {
+
+      const join = first.reduce((join, f) => {
+        next.forEach((col, i) => {
+          join[i] = [];
+          col.forEach(n => join.push([f, n]))
+        });
+        return join;
+      },[]);
+
+      super(select, join, where, stack, group);
+    }
   }
 
-  this.where = function(clause) {
-    return new query(selectFn, collection, (where || []).concat([clause]), orderBy, groupBy, failure);
+  return () => new Query();
+})();
+
+const set = [...(function*(n) { var l = n; while(n-- > 0) yield l - n; })(9)];
+
+const parity = (n) => !(n % 2) ? 'even' : 'odd';
+
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
   }
 
-  this.groupBy = function(g) {
-    return new query(selectFn, collection, where, orderBy, g, failure || (groupBy && "Duplicate GROUP BY"));    
-  }
+  var divisor = 2;
+  for(; number % divisor !== 0; divisor++);
+  return divisor === number;
+}
 
-  this.execute = function() {
-    if (failure) throw failure;
-    var filtered = where && where.reduce(function(arr, fn) { return collection.filter(fn); }, collection) || collection;
+const prime = (number) => isPrime(number) ? 'prime' : 'divisible';
+const lessThan = (lt) => (n) => n < lt ? `less than ${lt}` : `bigger than ${lt}`;
+const inGroup = (identifier) => (group) => group[0] === identifier;
 
-    console.log(filtered, selectFn)
-    return filtered.map(selectFn || defaultFn);
-  }
-};
+/*console.log
+(
+  query()
+    .from(set)
+    .groupBy(parity, prime)
+    .having(inGroup('even'))
+    .execute()
+)*/
 
 var teachers = [
-  {
-    teacherId: '1',
-    teacherName: 'Peter'
-  },
-  {
-    teacherId: '2',
-    teacherName: 'Anna'
-  }
+  { teacherId: '1', teacherName: 'Peter' },
+  { teacherId: '2', teacherName: 'Anna' }
 ];
-
 
 var students = [
-  {
-    studentName: 'Michael',
-    tutor: '1'
-  },
-  {
-    studentName: 'Rose',
-    tutor: '2'
-  }
+  { studentId: '1', studentName: 'Michael', tutor: '1' },
+  { studentId: '2', studentName: 'Rose', tutor: '2' }
 ];
 
-function teacherJoin(join) {
-  console.log(join)
-  return join[0].teacherId === join[1].tutor;
+var classes = [
+  { className: 'A', tutor: '1', student: '1' },
+  { className: 'A', tutor: '1', student: '2' },
+  { className: 'B', tutor: '2', student: '1' },
+  { className: 'B', tutor: '2', student: '2' }
+];
+
+const teacherJoin = (join) => join[0].teacherId === join[1].tutor && join[1].studentId === join[2].student;
+const student = (join) => ({ studentName: join[1].studentName, teacherName: join[0].teacherName });
+
+const data = [teachers, students, classes];
+
+for (const set of data) {
 }
 
-function student(join) {
-  return {studentName: join[1].studentName, teacherName: join[0].teacherName};
+const d = [[1, 2], [4, 5], [7, 8]];
+
+const cartesian_product = ([[head, ...tail], ...all]) => {
+  console.log(head, tail, all);
+
+  return head
+    ? [[head].concat(cartesian_product(all.concat([tail])))]
+    : [];
 }
 
-console.log(
-  [teachers, students].filter(teacherJoin)
-)
+var join = [];
+d[0].forEach(t => {
+  d[1].forEach(e => {
+    d[2].forEach(c => {
+      join.push([t, e, c]);
+    });
+  });
+});
 
-/*
-console.log (
-  query().select(student).from(teachers, students).where(teacherJoin).execute()
+console.log
+(
+  //join,
+  cartesian_product(d)
+  //query().from(teachers, students)/*.where(teacherJoin)*/.select().execute()
 )
-*/
